a44471b2b612ff85d388e1ab62497b401f352359
Danush Vithiyarth <danushvithiyarth@Danushs-MacBook-Pro.local>
2025-10-28 23:35:04 +0200

    Updates terraform script

--- Terraform_Script/Modules/machine-3.tf
+++ Terraform_Script/Modules/machine-3.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Machine-3" {
-  ami           = "ami-075449515af5df0d1"
+  ami           = "ami-0a716d3f3b16d290c"
   instance_type = "t3.micro"
   key_name      = "keyInTerminal"
   tags = {
