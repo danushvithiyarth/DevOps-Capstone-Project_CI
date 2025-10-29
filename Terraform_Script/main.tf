b3d8cdfb70ba86aa56bfee0d3d41fdeced1908a2
danushvithiyarth <158765030+danushvithiyarth@users.noreply.github.com>
2025-10-29 09:30:08 +0200

    Set up S3 backend for Terraform
    Configure Terraform backend to use S3 for state management.

--- Terraform_Script/main.tf
+++ Terraform_Script/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "danushvithiyarthjaiganesh"
+    key    = "terraform-capstoneproject.tf"
+    region = "eu-north-1"
+  }
+}
+
 provider "aws" {
   region = "eu-north-1"
 }
