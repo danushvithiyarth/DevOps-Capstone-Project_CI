2f47bcc7df8e415188904f37490c913528a624c7
danushvithiyarth <158765030+danushvithiyarth@users.noreply.github.com>
2025-10-29 20:26:32 +0200

    Update machine-3.tf

--- Terraform_Script/Modules/machine-3.tf
+++ Terraform_Script/Modules/machine-3.tf
@@ -34,6 +34,13 @@ resource "aws_security_group" "Machine-3-securitygroup" {
   }
 
   ingress {
+    from_port   = "8200"
+    to_port     = "8200"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = "9090"
     to_port     = "9090"
     protocol    = "tcp"
