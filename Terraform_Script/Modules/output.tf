ca6284bd7cdf1c65e5832c8000b68941d0744e2a
danushvithiyarth <158765030+danushvithiyarth@users.noreply.github.com>
2025-10-29 12:28:19 +0200

    Update output.tf

--- Terraform_Script/Modules/output.tf
+++ Terraform_Script/Modules/output.tf
@@ -1,7 +1,11 @@
-output "Machine-3-output" {
-  value = aws_instance.Machine-3.public_ip
+output "Machine-1-output" {
+  value = aws_instance.Machine-1.public_ip
 }
 
 output "Machine-2-output" {
   value = aws_instance.Machine-2.public_ip
+}
+
+output "Machine-3-output" {
+  value = aws_instance.Machine-3.public_ip
 }
