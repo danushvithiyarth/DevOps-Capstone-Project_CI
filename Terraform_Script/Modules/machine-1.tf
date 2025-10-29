912531f35ffe220201563fbc8a9c564f90e4671f
Danush Vithiyarth <danushvithiyarth@Danushs-MacBook-Pro.local>
2025-10-29 12:16:08 +0200

    Whole script configuration

--- Terraform_Script/Modules/machine-1.tf
+++ Terraform_Script/Modules/machine-1.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "Machine-2-securitygroup" {
   }
 
   ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = "3000"
     to_port     = "3000"
     protocol    = "tcp"
--- Terraform_Script/Modules/machine-3.tf
+++ Terraform_Script/Modules/machine-3.tf
@@ -1,8 +1,57 @@
 resource "aws_instance" "Machine-3" {
   ami           = "ami-0a716d3f3b16d290c"
-  instance_type = "t3.micro"
+  instance_type = "t3.large"
   key_name      = "keyInTerminal"
+  subnet_id     = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.Machine-3-securitygroup.id]
+  associate_public_ip_address = true
+  root_block_device {
+    volume_size = 35
+    volume_type = "gp3"
+  }
   tags = {
-    Name = "Deployment machine"
+    Name = "Deployment Machine-3"
   }
 }
+
+resource "aws_security_group" "Machine-3-securitygroup" {
+  name        = "Machine-3-securitygroup"
+  description = "SG for Machine-3"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "9090"
+    to_port     = "9090"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "3000"
+    to_port     = "3000"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
