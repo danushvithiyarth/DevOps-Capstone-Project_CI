b25fa73aaa62f9f0ba49a9d4ef02f99c54017b3f
danushvithiyarth <158765030+danushvithiyarth@users.noreply.github.com>
2025-10-29 12:28:46 +0200

    Add output for public_IP_Machine-1 and reorder outputs

--- Terraform_Script/output.tf
+++ Terraform_Script/output.tf
@@ -1,8 +1,11 @@
-output "public_IP_Machine-3" {
-  value = module.module.Machine-3-output
+output "public_IP_Machine-1" {
+  value = module.module.Machine-1-output
 }
-
 
 output "public_IP_Machine-2" {
   value = module.module.Machine-2-output
+}
+
+output "public_IP_Machine-3" {
+  value = module.module.Machine-3-output
 }
