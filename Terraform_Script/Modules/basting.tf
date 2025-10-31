509113e2de487428cbd185506fdc5d0ca95bab25
danushvithiyarth <158765030+danushvithiyarth@users.noreply.github.com>
2025-10-31 12:55:17 +0200

    Update basting.tf

--- Terraform_Script/Modules/basting.tf
+++ Terraform_Script/Modules/basting.tf
@@ -8,11 +8,11 @@ resource "aws_security_group_rule" "allow_ec2_to_eks_api" {
   security_group_id        = "sg-070ea8267cd1a244e"
 }
 
-resource "aws_security_group_rule" "allow_api_to_vault_injector" {
-  description              = "Allow EKS API server to reach Vault Agent Injector on worker nodes"
+resource "aws_security_group_rule" "allow_control_plane_to_nodes" {
+  description              = "Allow EKS Control Plane to reach Vault Injector webhook"
   type                     = "ingress"
-  from_port                = 31206
-  to_port                  = 31206
+  from_port                = 443
+  to_port                  = 443
   protocol                 = "tcp"
   security_group_id        = "sg-08fa8e70deafa4e04"        # Worker nodes SG
   source_security_group_id = "sg-070ea8267cd1a244e"        # EKS control plane SG
