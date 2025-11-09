cab7e94d40357d97a54e1ad6398c0ddd23a2fd52
Danush Vithiyarth <danushvithiyarth@Danushs-MacBook-Pro.local>
2025-11-09 10:04:12 +0200

    Updated script

--- Terraform_Script/Modules/local_ip.tf
+++
@@ -1,8 +0,0 @@
-data "http" "my_ip" {
-  url = "https://ifconfig.me"
-}
-
-locals {
-  my_ip_cidr = "${chomp(data.http.my_ip.response_body)}/32"
-}
-
--- Terraform_Script/Modules/machine-1.tf
+++ Terraform_Script/Modules/machine-1.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "Machine-1-securitygroup" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [local.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
